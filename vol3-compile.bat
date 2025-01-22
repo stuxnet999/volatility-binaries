4a916c88cfb6455023601846acafaf8344d7e336
stuxnet999 <abhiram1999@gmail.com>
2025-01-22 23:00:40 +0530

    Pushing vol3 2.11.0
    Pushed vol3 2.11.0 and also update vol3-compile.bat to use pyinstaller 6.10.0

--- vol3-compile.bat
+++ vol3-compile.bat
@@ -3,7 +3,7 @@ echo This small batch file will attempt to compile volatility3.
 echo The main requirements are to have python 3.10 and Microsoft Visual C++ 14.0 or greater.
 timeout /t 5
 pip install -r requirements.txt
-pip install pyinstaller==6.1.0
+pip install pyinstaller==6.10.0
 pyinstaller vol.spec
 pyinstaller volshell.spec
 move /y dist\vol.exe dist\vol3.exe
